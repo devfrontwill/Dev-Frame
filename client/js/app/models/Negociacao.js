8b81e80e244418f22bfe916a7247ca2aa8ce197a
Wiliam Roza <devfrontwill@gmail.com>
2022-10-10 18:01:09 -0300

    trabalhando com classes e get

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,24 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get Volume() {
+        this._quantidade * this._valor;
+    }
+
+    get Data() {
+        return this._data;
+    }
+
+    get Quantidade() {
+        return this._quantidade;
+    }
+
+    get Valor() {
+        return this._valor;
+    }
+}
