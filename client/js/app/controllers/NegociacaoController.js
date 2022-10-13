4bb5ceda7a605c56a5865b82ff0377f170d3de39
Wiliam Roza <devfrontwill@gmail.com>
2022-10-12 21:35:22 -0300

    trabalhando com POO em JS

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,16 @@
+class NegociacaoController {
+
+    constructor() {
+        
+        let $ = document.querySelector.bind(document);    
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+
+    }
+    
+    adiciona (event) {
+        event.preventDefault();        
+    }
+
+}
