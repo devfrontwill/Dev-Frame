bb1717a3cfec0f23d231dccd4bcd143e4078d2b1
Wiliam Roza <devfrontwill@gmail.com>
2022-10-26 19:11:51 -0300

    refatorando o codigo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,12 +1,10 @@
 class NegociacaoController {
 
-    constructor() {
-        
+    constructor() {        
         let $ = document.querySelector.bind(document);    
         this.inputData = $('#data');
         this.inputQuantidade = $('#quantidade');
         this.inputValor = $('#valor');
-
     }
     
     adiciona (event) {
