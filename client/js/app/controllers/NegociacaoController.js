63a62b750e8c32eb6701781fbf834dd5cbb67550
Wiliam Roza <devfrontwill@gmail.com>
2022-11-01 13:06:45 -0300

    Adicionado nova funcionalidade

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,6 +8,23 @@ class NegociacaoController {
     }
     
     adiciona (event) {
-        event.preventDefault();        
+        event.preventDefault();     
+        
+        let data = new Date(
+            ...this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice % 2)
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        let diaMesAno = negociacao.data.getDate()
+         + '/' + (negociacao.data.getMonth() +1)
+         + '/' + negociacao.data.getFullYear;
+        console.log(diaMesAno);
     }
 }
